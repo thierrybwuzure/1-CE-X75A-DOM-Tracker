c32c5dcf32c1752431be7d2b016e3d3332ae6a44
Thierry Bwuzure <bwuzure@gmail.com>
2026-03-11 20:35:53 +0100

    Etape 3 Pause

--- tracker.js
+++ tracker.js
@@ -1,17 +1,24 @@
 let btn_sun = document.getElementById('btn-sun');
-let cnt = document.getElementById('sun-value');
+let cnt1 = document.getElementById('sun-value');
 
 let btn_water = document.getElementById('btn-water');
-let cnt1 = document.getElementById('water-value');
+let cnt2 = document.getElementById('water-value');
+
+let btn_break = document.getElementById('btn-break');
+let cnt3 = document.getElementById('break-value');
 
 let total = document.getElementById('val_total');
 
 btn_sun.addEventListener('click', () => {
-    cnt.innerText = parseInt(cnt.innerText) + 1;
+    cnt1.innerText = parseInt(cnt1.innerText) + 1;
 });
 
 btn_water.addEventListener('click', () => {
-    cnt1.innerText = parseInt(cnt1.innerText) + 1;
+    cnt2.innerText = parseInt(cnt2.innerText) + 1;
+});
+
+btn_break.addEventListener('click', () => {
+    cnt3.innerText = parseInt(cnt3.innerText) + 1;
 });
 
 console.log(btn_sun, btn_water);
