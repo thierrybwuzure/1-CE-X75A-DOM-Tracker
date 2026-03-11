1ca7818b82d52e399ea9ca572716ead603b2f7ff
Thierry Bwuzure <bwuzure@gmail.com>
2026-03-11 20:55:49 +0100

    Etape 3 Total

--- tracker.js
+++ tracker.js
@@ -7,18 +7,22 @@ let cnt2 = document.getElementById('water-value');
 let btn_break = document.getElementById('btn-break');
 let cnt3 = document.getElementById('break-value');
 
-let total = document.getElementById('val_total');
+let goal_message = document.getElementById('goal-message');
+let cnt4 = document.getElementById('total-value');
 
 btn_sun.addEventListener('click', () => {
     cnt1.innerText = parseInt(cnt1.innerText) + 1;
+    cnt4.innerText = parseInt(cnt4.innerText) + 1;
 });
 
 btn_water.addEventListener('click', () => {
     cnt2.innerText = parseInt(cnt2.innerText) + 1;
+    cnt4.innerText = parseInt(cnt4.innerText) + 1;
 });
 
 btn_break.addEventListener('click', () => {
     cnt3.innerText = parseInt(cnt3.innerText) + 1;
+    cnt4.innerText = parseInt(cnt4.innerText) + 1;
 });
 
-console.log(btn_sun, btn_water);
+console.log(btn_sun, btn_water, btn_break, goal_message);
