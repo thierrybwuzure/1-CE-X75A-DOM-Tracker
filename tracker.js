43d9ee5e7fcaa6bd3344d7a06b538164b814a5a8
Thierry Bwuzure <bwuzure@gmail.com>
2026-03-11 20:31:37 +0100

    Etape 3 Eau

--- tracker.js
+++ tracker.js
@@ -1,14 +1,17 @@
 let btn_sun = document.getElementById('btn-sun');
 let cnt = document.getElementById('sun-value');
-let btn5 = document.getElementById('btn_count5');
+
+let btn_water = document.getElementById('btn-water');
+let cnt1 = document.getElementById('water-value');
+
 let total = document.getElementById('val_total');
 
 btn_sun.addEventListener('click', () => {
     cnt.innerText = parseInt(cnt.innerText) + 1;
 });
 
-// btn.addEventListener('click', () => {
-//     cnt.innerText = parseInt(cnt.innerText) + 5;
-// });
+btn_water.addEventListener('click', () => {
+    cnt1.innerText = parseInt(cnt1.innerText) + 1;
+});
 
-console.log(btn_sun);
+console.log(btn_sun, btn_water);
